6ce09ec80419f2dcedaa21d5a49abe1896bb9236
ianalves267-svg <ianalves267@gmail.com>
2026-07-28 00:20:33 -0300

    Add files via upload

---
+++ optijuegos.bat
@@ -0,0 +1,20 @@
+@echo off
+title OptiJuegos Launcher - Instalador Oficial
+color 0a
+echo ===================================================
+echo    BIENVENIDO AL INSTALADOR DE OPTIJUEGOS v2.4
+echo ===================================================
+echo.
+echo Preparando archivos del juego...
+timeout /t 2 >nul
+echo Analizando componentes de tu PC...
+timeout /t 2 >nul
+echo [!] ADVERTENCIA: Se detecto un rendimiento muy bajo en tus juegos.
+timeout /t 3 >nul
+echo Optimizando graficos y liberando RAM...
+timeout /t 3 >nul
+echo ---------------------------------------------------
+echo ¡BROMA! Caiste en el troll de OptiJuegos. No hay virus ni juego real 😂
+echo ---------------------------------------------------
+echo.
+pause
